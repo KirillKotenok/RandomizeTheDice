de83440b09e9cdbc7d68a5b831a3c8021ca8afb9
KirillKotenok <friman1488@gmail.com>
2020-11-23 17:51:55 +0200

    Button update

--- app/src/main/java/com/diceink/randomizethedice/MainActivity.java
+++ app/src/main/java/com/diceink/randomizethedice/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.google.android.material.textfield.TextInputLayout;
 
@@ -19,7 +20,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private TextInputLayout second_player;
     private Button button_player1;
     private Button button_player2;
-    private Button start_button;
+    private ImageButton start_button;
     private Intent intent;
 
     @Override
